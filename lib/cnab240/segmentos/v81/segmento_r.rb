fd9a9cee78471f779291a05691386fa115629113
Kivanio Barbosa <kivanio@gmail.com>
2015-05-19 16:58:07 -0400

    fix v81 R segment

--- lib/cnab240/segmentos/v81/segmento_r.rb
+++ lib/cnab240/segmentos/v81/segmento_r.rb
@@ -7,16 +7,18 @@ module Cnab240
       lstring :controle_banco, length: 3, pad_byte: '0'
       lstring :controle_lote, length: 4, pad_byte: '0'
       string :controle_registro, length: 1, initial_value: '3', pad_byte: '0'
+
       lstring :servico_numero_registro, length: 5, pad_byte: '0'
       string :servico_codigo_segmento, length: 1, initial_value: 'R', pad_byte: ' '
       lstring :servico_tipo_movimento, length: 1, pad_byte: ' '
       lstring :servico_codigo_movimento, length: 2, pad_byte: '0'
+
       lstring :codigo_desconto_2, length: 1, pad_byte: '0' #'1'= Valor Fixo Até a Data Informada, '2'= Percentual Até a Data Informada
       lstring :data_desconto_2, length: 8, pad_byte: '0'
       lstring :valor_desconto_2, length: 15, pad_byte: '0'
       lstring :codigo_desconto_3, length: 1, pad_byte: '0' #'1'= Valor Fixo Até a Data Informada, '2'= Percentual Até a Data Informada
-      lstring :data_desconto_4, length: 8, pad_byte: '0'
-      lstring :valor_desconto_4, length: 15, pad_byte: '0'
+      lstring :data_desconto_3, length: 8, pad_byte: '0'
+      lstring :valor_desconto_3, length: 15, pad_byte: '0'
       lstring :codigo_multa, length: 1, pad_byte: '0' #'1'= Valor Fixo, '2'= Percentual
       lstring :data_multa, length: 8, pad_byte: '0'
       lstring :valor_multa, length: 15, pad_byte: '0'
