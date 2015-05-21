d8b965edc97beb47227418866c97bde1305e68b5
Kivanio Barbosa <kivanio@gmail.com>
2015-05-21 14:10:38 -0400

    use same var name as other trailers

--- lib/cnab240/lote/v40/boletos/trailer.rb
+++ lib/cnab240/lote/v40/boletos/trailer.rb
@@ -10,17 +10,15 @@ module Cnab240
           lstring :controle_registro, length: 1, initial_value: '5', pad_byte: '0'
 
           lstring :cnab_g004_1, length: 9, pad_byte: '0'
-          lstring :qtde_registros, length: 6, pad_byte: '0'
+          lstring :totais_qtde_registros, length: 6, pad_byte: '0'
           lstring :qtde_cobranca_simples, length: 6, pad_byte: '0'
           lstring :total_cobranca_simples, length: 17, pad_byte: '0'
-
           lstring :qtde_cobranca_vinculada, length: 6, pad_byte: '0'
           lstring :total_cobranca_vinculada, length: 17, pad_byte: '0'
           lstring :qtde_cobranca_caucionada, length: 6, pad_byte: '0'
           lstring :total_cobranca_caucionada, length: 17, pad_byte: '0'
           lstring :qtde_cobranca_descontada, length: 6, pad_byte: '0'
           lstring :total_cobranca_descontada, length: 17, pad_byte: '0'
-
 
           string :aviso, length: 8, pad_byte: ' '
           string :cnab_g004_2, length: 117, pad_byte: ' '
